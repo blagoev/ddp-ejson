2cd0230a864e43086e68eb723dd5cfca22ef7dca
blagoev <lubo@blagoev.com>
2015-11-29 21:56:41 +0200

    remove crypto dependency

--- objectid.js
+++ objectid.js
@@ -8,25 +8,17 @@
 //
 // All contributions licensed under the MIT license: see LICENSE file in this directory
 
-nodeCrypto = require('crypto');
-
-randomHexString = function (digits) {
-  var self = this;
-  var numBytes = Math.ceil(digits / 2);
-  var bytes;
-  // Try to get cryptographically strong randomness. Fall back to
-  // non-cryptographically strong if not available.
-  try {
-    bytes = nodeCrypto.randomBytes(numBytes);
-  } catch (e) {
-    // XXX should re-throw any error except insufficient entropy
-    bytes = nodeCrypto.pseudoRandomBytes(numBytes);
+randomHexString = function(digits) {
+  var result = Math.floor(Math.random()*16777215).toString(16);
+  
+  
+  while (result.length < digits)
+  {
+    result = result + Math.floor(Math.random()*16777215).toString(16);  
   }
-  var result = bytes.toString("hex");
-  // If the number of digits is odd, we'll have generated an extra 4 bits
-  // of randomness, so we need to trim the last digit.
+  
   return result.substring(0, digits);
-};
+}
 
 looksLikeObjectID = function (str) {
   return str.length === 24 && str.match(/^[0-9a-f]*$/);
